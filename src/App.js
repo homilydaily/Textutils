47db415494ee54b0a0eb893b31290f58d4aa6e64
homilydaily <117662954+homilydaily@users.noreply.github.com>
2022-11-08 18:22:33 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import React from "react";
-// import {
-//   BrowserRouter,
-//   Route,
-//   Routes
-// } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
 
 
 function App() {
   return (
     <>
-    {/* <BrowserRouter> */}
+    <BrowserRouter>
 <Navbar title="TextUtils"/>
 <div className="container my-3">
-<TextForm heading="Enter the text to analyze below"/>
 
-        {/* <Routes>
+
+        <Routes>
           <Route exact path="/about" element={<About />}/>
-          <Route exact path="/" element={}/>
-        </Routes> */}
+          <Route exact path="/" element={<TextForm heading="Enter the text to analyze below"/>}/>
+        </Routes>
 
        
 </div>
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
 
 
 
